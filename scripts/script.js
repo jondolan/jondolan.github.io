3193ba74810bc29c2ac693e58ec325886d4956e6
Jon Dolan <jondolan33@gmail.com>
2014-05-29 22:11:48 -0400

    Mobile optimization (seemingly) complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,187 @@
 var up = true;
-function moveDown(element, top)
+function moveDown(element, bottom)
 {
 	if (up == true)
 	{
-		if (top == undefined)
+		if (bottom == undefined)
 		{
-			if (element.style.top == '')
-				top = 110;
+			if (element.style.bottom == '')
+				bottom = -10;
 			else
-				top = parseInt(element.style.top);
+				bottom = parseInt(element.style.bottom);
 		}
-		else if (top >= 420)
+		else if (bottom <= -300)
 		{
 			document.getElementById('about').style.display = 'block';
 			up = false;
 			return;
 		}
-		element.style.backgroundPosition = '0px ' + top + 'px';
-		setTimeout(function(){ moveDown(element,top+10)}, 15);
+		element.style.bottom = bottom + 'px';
+		setTimeout(function(){ moveDown(element,bottom-8)}, 15);
 	}
 	else
 		setTimeout(function(){ moveDown(element)}, 100);
 }
-function moveUp(element, top)
+function moveUp(element, bottom, upTo)
 {
+	if (upTo == null)
+		upTo = -10;
 	if (up == false)
 	{
 		document.getElementById('about').style.display = 'none';
-		if (parseInt(top) == 0 || top == undefined)
-			top = 420;
-		else if (parseInt(top) <= 120)
+		if (parseInt(bottom) == 0 || bottom == undefined)
+			bottom = -300;
+		else if (bottom >= upTo)
 		{
-			element.style.backgroundPosition = '0px 110px';
+			element.style.bottom = upTo + 'px';
 			up = true;
 			return;
 		}
-		element.style.backgroundPosition = '0px ' + top + 'px';
-		setTimeout(function(){ moveUp(element, top-10)}, 15);
+		element.style.bottom = bottom + 'px';
+		setTimeout(function(){ moveUp(element, bottom+8, upTo)}, 15);
 	}
 	else
-		setTimeout(function(){ moveUp(element)}, 100);
+		setTimeout(function(){ moveUp(element, 0, upTo)}, 100);
 }
+// TOUCH-EVENTS SINGLE-FINGER SWIPE-SENSING JAVASCRIPT
+// Courtesy of PADILICIOUS.COM and MACOSXAUTOMATION.COM
+
+// this script can be used with one or more page elements to perform actions based on them being swiped with a single finger
+
+var triggerElementID = null; // this variable is used to identity the triggering element
+var fingerCount = 0;
+var startX = 0;
+var startY = 0;
+var curX = 0;
+var curY = 0;
+var deltaX = 0;
+var deltaY = 0;
+var horzDiff = 0;
+var vertDiff = 0;
+var minLength = 72; // the shortest distance the user may swipe
+var swipeLength = 0;
+var swipeAngle = null;
+var swipeDirection = null;
+
+// The 4 Touch Event Handlers
+
+// NOTE: the touchStart handler should also receive the ID of the triggering element
+// make sure its ID is passed in the event call placed in the element declaration, like:
+// <div id="picture-frame" ontouchstart="touchStart(event,'picture-frame');"  ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);">
+
+function touchStart(event,passedName) {
+	// disable the standard ability to select the touched object
+	event.preventDefault();
+	// get the total number of fingers touching the screen
+	fingerCount = event.touches.length;
+	// since we're looking for a swipe (single finger) and not a gesture (multiple fingers),
+	// check that only one finger was used
+	if ( fingerCount == 1 ) {
+		// get the coordinates of the touch
+		startX = event.touches[0].pageX;
+		startY = event.touches[0].pageY;
+		// store the triggering element ID
+		triggerElementID = passedName;
+	} else {
+		// more than one finger touched so cancel
+		touchCancel(event);
+	}
+}
+
+function touchMove(event) {
+	event.preventDefault();
+	if ( event.touches.length == 1 ) {
+		curX = event.touches[0].pageX;
+		curY = event.touches[0].pageY;
+	} else {
+		touchCancel(event);
+	}
+}
+
+function touchEnd(event) {
+	event.preventDefault();
+	// check to see if more than one finger was used and that there is an ending coordinate
+	if ( fingerCount == 1 && curX != 0 ) {
+		// use the Distance Formula to determine the length of the swipe
+		swipeLength = Math.round(Math.sqrt(Math.pow(curX - startX,2) + Math.pow(curY - startY,2)));
+		// if the user swiped more than the minimum length, perform the appropriate action
+		if ( swipeLength >= minLength ) {
+			caluculateAngle();
+			determineSwipeDirection();
+			processingRoutine();
+			touchCancel(event); // reset the variables
+		} else {
+			touchCancel(event);
+		}	
+	} else {
+		touchCancel(event);
+	}
+}
+
+function touchCancel(event) {
+	// reset the variables back to default values
+	fingerCount = 0;
+	startX = 0;
+	startY = 0;
+	curX = 0;
+	curY = 0;
+	deltaX = 0;
+	deltaY = 0;
+	horzDiff = 0;
+	vertDiff = 0;
+	swipeLength = 0;
+	swipeAngle = null;
+	swipeDirection = null;
+	triggerElementID = null;
+}
+
+function caluculateAngle() {
+	var X = startX-curX;
+	var Y = curY-startY;
+	var Z = Math.round(Math.sqrt(Math.pow(X,2)+Math.pow(Y,2))); //the distance - rounded - in pixels
+	var r = Math.atan2(Y,X); //angle in radians (Cartesian system)
+	swipeAngle = Math.round(r*180/Math.PI); //angle in degrees
+	if ( swipeAngle < 0 ) { swipeAngle =  360 - Math.abs(swipeAngle); }
+}
+
+function determineSwipeDirection() {
+	if ( (swipeAngle <= 45) && (swipeAngle >= 0) ) {
+		swipeDirection = 'left';
+	} else if ( (swipeAngle <= 360) && (swipeAngle >= 315) ) {
+		swipeDirection = 'left';
+	} else if ( (swipeAngle >= 135) && (swipeAngle <= 225) ) {
+		swipeDirection = 'right';
+	} else if ( (swipeAngle > 45) && (swipeAngle < 135) ) {
+		swipeDirection = 'down';
+	} else {
+		swipeDirection = 'up';
+	}
+}
+
+function processingRoutine() {
+	var swipedElement = document.getElementById(triggerElementID);
+	if ( swipeDirection == 'left' ) {
+		// REPLACE WITH YOUR ROUTINES
+		swipedElement.style.backgroundColor = 'orange';
+	} else if ( swipeDirection == 'right' ) {
+		// REPLACE WITH YOUR ROUTINES
+		swipedElement.style.backgroundColor = 'green';
+	} else if ( swipeDirection == 'up' ) {
+		// REPLACE WITH YOUR ROUTINES
+		swipedElement.style.backgroundColor = 'maroon';
+	} else if ( swipeDirection == 'down' ) {
+		// REPLACE WITH YOUR ROUTINES
+		swipedElement.style.backgroundColor = 'purple';
+	}
+}
+function processingRoutine() {
+				var swipedElement = document.getElementById(triggerElementID);
+				//if ( swipeDirection == 'left' ) {
+				//} else if ( swipeDirection == 'right' ) {
+				//} else 
+				if ( swipeDirection == 'up' ) {
+					moveUp(document.getElementById('image'));
+				} else if ( swipeDirection == 'down' ) {
+					moveDown(document.getElementById('image'));
+				}
+			}
